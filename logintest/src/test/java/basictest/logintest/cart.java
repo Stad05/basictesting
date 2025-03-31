49ec8f33335d8c8c9a622b1f8884703fd7c98e5d
Admin <Admin@DESKTOP-K5LDBGH>
2025-03-31 13:19:54 +0530

    sec commit

--- logintest/src/test/java/basictest/logintest/cart.java
+++ logintest/src/test/java/basictest/logintest/cart.java
@@ -1,12 +1,10 @@
 package basictest.logintest;
 
-public class loginsearch {
+public class cart {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		System.out.println("in test");
 	}
 
 }
